f908ca5ef4abb25a9651020688bb7e2cf583713e
PiyushBadhe <badhepiyuraj1997@gmail.com>
2022-10-04 13:48:33 +0530

    Added types of variables and their characteristics

---
+++ variables_in_java.java
@@ -0,0 +1,16 @@
+/** ******************************
+ *  | Types of Variables in Java |
+ *  ******************************
+ * 
+ * 1. Instance Variables
+ *  - Instance Variables are declared in a class but outside a method declaration
+ *  - Declared without using 'static' keyword
+ * 
+ * 2. Static Variables
+ *  - Initialised only once at the start of program execution
+ *  - These variables should be initialised first before initialisation of any instance variable
+ *  - Defined in a class but outside method declaration
+ * 
+ * 3. Local Variables
+ *  - Declared inside the body of the method
+ */
